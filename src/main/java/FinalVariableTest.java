ad256506b10a536c7d9b4de7457c0ac669cd9e2b
GraphTree <graphnodesed@gmail.com>
2022-09-23 17:24:30 +0900

    testing access modifier (final, static)

---
+++ src/main/java/FinalVariableTest.java
@@ -0,0 +1,13 @@
+public class FinalVariableTest {
+
+    private static int classCounter = 0;
+    public final int instanceNumber;
+    private final String name;
+
+    public FinalVariableTest(String name) {
+        this.name = name;
+        classCounter++;
+        instanceNumber=classCounter;
+        System.out.println(name + " created, instance is " + instanceNumber);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 public class Main
 {
     public static void main(String[] args) {
-        StaticTest TestA = new StaticTest("a");
-        System.out.println(TestA.getNumInstances());
-        StaticTest TestB = new StaticTest("b");
-        System.out.println(TestB.getNumInstances());
+        FinalVariableTest one = new FinalVariableTest("one");
+        FinalVariableTest two = new FinalVariableTest("two");
 
     }
 }
