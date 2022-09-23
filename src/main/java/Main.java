536bd3510c01d16a6432607ff0e02a290c5a14ec
GraphTree <graphnodesed@gmail.com>
2022-09-23 16:50:01 +0900

    static variable test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,10 @@
 public class Main
 {
     public static void main(String[] args) {
-        Account timsAccount = new Account("Tim");
-        timsAccount.deposit(1000);
-        timsAccount.withdraw(500);;
-        timsAccount.withdraw(-200);;
-        timsAccount.deposit(-20);
-        timsAccount.calculateBalance();
+        StaticTest TestA = new StaticTest("a");
+        System.out.println(TestA.getNumInstances());
+        StaticTest TestB = new StaticTest("b");
+        System.out.println(TestB.getNumInstances());
 
-        System.out.println(timsAccount.getBalance());
     }
 }
---
+++ src/main/java/StaticTest.java
@@ -0,0 +1,13 @@
+public class StaticTest {
+    private int numInstances = 0;
+    private String name;
+
+    public StaticTest(String name) {
+        this.name = name;
+        numInstances++;
+    }
+
+    public int getNumInstances() {
+        return numInstances;
+    }
+}
